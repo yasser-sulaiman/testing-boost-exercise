22762bd718bae760b1313fc3eaeaf623c37c3961
Yasser Sulaiman <yassersulaiman.is@gmail.com>
2023-02-09 01:56:33 +0100

    trying another test

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -4,30 +4,17 @@
 
 using namespace Eigen;
 
-struct MatrixIOFixture {
-  MatrixIOFixture()
-  {
-    testMatrix = MatrixXd(3, 3);
 
-    testMatrix << 0.680375, 0.59688, -0.329554,
-        -0.211234, 0.823295, 0.536459,
-        0.566198, -0.604897, -0.444451;
-  }
+BOOST_AUTO_TEST_SUITE(MatrixIOTests)
 
-  MatrixXd testMatrix;
-};
-
-BOOST_FIXTURE_TEST_SUITE(MatrixIOTests, MatrixIOFixture, *boost::unit_test::tolerance(1e-16))
-
-BOOST_AUTO_TEST_CASE(MyOnlyTest)
-
+BOOST_AUTO_TEST_CASE(OpenDataDimension)
 {
-  MatrixXd loadedMatrix = matrixIO::openData("../data/m3.csv", 3);
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      BOOST_TEST(testMatrix(i, j) == loadedMatrix(i, j));
-    }
-  }
+    int rowsNumber = 3;
+    int columnsNumber = 3;
+    MatrixXd LoadedMatrix = matrixIO::openData("../data/m3.csv", 3);
+    BOOST_TEST(LoadedMatrix.rows() == rowsNumber);
+    BOOST_TEST(LoadedMatrix.cols() == columnsNumber);
 }
+
 
 BOOST_AUTO_TEST_SUITE_END()
