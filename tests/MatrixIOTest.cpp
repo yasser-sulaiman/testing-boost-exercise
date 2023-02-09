ada75e2c3a87647ba37c085ec0b274b5348b06dd
Yasser Sulaiman <yassersulaiman.is@gmail.com>
2023-02-09 01:38:36 +0100

    trying to fix the test

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -7,14 +7,14 @@ using namespace Eigen;
 struct MatrixIOFixture {
   MatrixIOFixture()
   {
-    expectedMatrix = MatrixXd(3, 3);
+    testMatrix = MatrixXd(3, 3);
 
-    expectedMatrix << 0.680375, 0.59688, -0.329554,
+    testMatrix << 0.680375, 0.59688, -0.329554,
         -0.211234, 0.823295, 0.536459,
         0.566198, -0.604897, -0.444451;
   }
 
-  MatrixXd expectedMatrix;
+  MatrixXd testMatrix;
 };
 
 BOOST_FIXTURE_TEST_SUITE(MatrixIOTests, MatrixIOFixture)
@@ -22,10 +22,10 @@ BOOST_FIXTURE_TEST_SUITE(MatrixIOTests, MatrixIOFixture)
 BOOST_AUTO_TEST_CASE(MyOnlyTest)
 
 {
-  MatrixXd readMatrix = matrixIO::openData("../data/m3.csv", 3);
+  MatrixXd loadedMatrix = matrixIO::openData("../data/m3.csv", 3);
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      BOOST_TEST(expectedMatrix(i, j) == readMatrix(i, j));
+      BOOST_TEST(testMatrix(i, j) == loadedMatrix(i, j));
     }
   }
 }
